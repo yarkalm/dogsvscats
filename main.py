c6d85c497bd672b0e542b574714aebbf869e6c5b
Yaroslav Kalmykov <99351220+yarkalm@users.noreply.github.com>
2023-09-01 11:44:03 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from classifier import DogCatClassifier
+from preprocessing import train_loader, val_loader
+
+
+if __name__ == '__main__' :
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = DogCatClassifier().to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+
+    def train_model(model, criterion, optimizer, num_epochs=10):
+        for epoch in range(num_epochs):
+            model.train()
+            running_loss = 0.0
+            for i, (inputs, labels) in enumerate(train_loader):
+                #inputs, labels = inputs.to('cpu'), labels.to('cpu')
+                inputs, labels = next(iter(train_loader))
+                optimizer.zero_grad()
+
+                # Forward pass and compute loss
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+
+                # Backward pass and update weights
+                loss.backward()
+                optimizer.step()
+
+                running_loss += loss.item()
+
+            print(f'Epoch {epoch + 1}/{num_epochs}, Loss: {running_loss / len(train_loader)}')
+
+    def evaluate_model(model, dataloader):
+        model.eval()
+        correct = 0
+        total = 0
+
+        with torch.no_grad():
+            for data in dataloader:
+                images, labels = data
+                images, labels = images.to(device), labels.to(device)
+                outputs = model(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+        print(f'Accuracy on the evaluation set: {100 * correct / total:.2f}%')
+
+
+
+    train_model(model, criterion, optimizer, num_epochs=8)
+    evaluate_model(model, val_loader)
--- preprocessing.py
+++ preprocessing.py
@@ -1,17 +1,42 @@
+import cv2
+import copy
+import matplotlib.pyplot as plt
 import torch
 import torchvision
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+import numpy as np
 import torchvision.transforms as transforms
-# Data augmentation and normalisation for training
-transform = transforms.Compose([
-    transforms.RandomResizedCrop(224),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
 
-train_dataset = torchvision.datasets.ImageFolder(root= './data/train', transform=transform)
+# Data augmentation and normalisation for training
+
+base_augmentations = A.Compose(
+    [
+        A.SmallestMaxSize(max_size=160),
+        A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.05, rotate_limit=15, p=0.5),
+        A.RandomCrop(height=128, width=128),
+        A.RGBShift(r_shift_limit=15, g_shift_limit=15, b_shift_limit=15, p=0.5),
+        A.RandomBrightnessContrast(p=0.5),
+        A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+        ToTensorV2(),
+    ]
+)
+
+
+class Transforms:
+    def __init__(self, transforms: A.Compose):
+        self.transforms = transforms
+
+    def __call__(self, img, *args, **kwargs):
+        return self.transforms(image=np.array(img))
+
+
+
+
+
+train_dataset = torchvision.datasets.ImageFolder(root='./dataset/train', transform=Transforms(transforms=A.Compose(base_augmentations)))
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=4)
 
-val_dataset = torchvision.datasets.ImageFolder(root= './data/validation', transform=transform)
+val_dataset = torchvision.datasets.ImageFolder(root='./dataset/validation', transform=Transforms(transforms=A.Compose(base_augmentations)))
 val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=64, shuffle=False, num_workers=4)
 
