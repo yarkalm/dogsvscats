b1f841a6c2af1458bada4a2034f6cf9ff2e4f053
Yaroslav Kalmykov <99351220+yarkalm@users.noreply.github.com>
2023-09-03 02:25:05 +0500

    Add files via upload

--- evaluate.py
+++ evaluate.py
@@ -3,25 +3,10 @@ import cv2
 import torch
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
 
 
-
-# Класс создания датасета для валидации
-class DogsVSCatsInferenceDataset(Dataset):
-    def __init__(self, images_filepaths, transform=None):
-        self.images_filepaths = images_filepaths
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.images_filepaths)
-
-    def __getitem__(self, idx):
-        image_filepath = self.images_filepaths[idx]
-        image = cv2.imread(image_filepath)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        if self.transform is not None:
-            image = self.transform(image=image)["image"]
-        return image
 
 
 # вывод изображений
@@ -52,3 +37,22 @@ def predict(model, params, test_loader):
             predicted_labels += ["Cat" if is_cat else "Dog" for is_cat in predictions]
 
     return predicted_labels
+
+def conf_matrix(model, params, loader):
+    model = model.eval()
+    y_true = []
+    y_pred = []
+    with torch.no_grad():
+        for images, labels in loader:
+            images = images.to(params["device"], non_blocking=True)
+            labels = labels.to(params["device"], non_blocking=True)
+            output = model(images)
+
+            y_pred.extend(torch.sigmoid(output) >= 0.5)[:, 0].cpu().numpy()
+            y_true.extend(labels.cpu().numpy())
+    
+    print(confusion_matrix(y_true, y_pred))
+    print("Precision of the Model :\t" + str(precision_score(y_true, y_pred)))
+    print("Recall of the Model    :\t" + str(recall_score(y_true, y_pred)))
+    print("F1 Score of the Model  :\t" + str(f1_score(y_true, y_pred)))
+
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os
 import cv2
 import torch
+import itertools
+import numpy as np
 from pathlib import Path
+import matplotlib.pyplot as plt
 from preprocessing import create_loader
 from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
 from evaluate import predict, display_image_grid
@@ -33,31 +36,95 @@ if __name__ == '__main__':
         "epochs": 2,
     }
 
-    # Создание экземпляра модели
+        # Создание экземпляра модели
     model, criterion, optimizer = create_model(params)
     # Создание датасетов
-    train_loader, val_loader, test_loader = create_loader(correct_images_filepaths)
+    train_loader, train_dataset, val_loader, val_dataset, test_loader, test_dataset, y_true_dataset, y_true_loader = create_loader(correct_images_filepaths)
 
     # Запуск обучения
+    history_training = {'accuracy':[],'loss':[]}
+    history_validation = {'accuracy':[],'loss':[]}
     for epoch in range(1, params["epochs"] + 1):
         history_train = train(train_loader, model, criterion, optimizer, epoch, params)
-        history_test = validate(val_loader, model, criterion, epoch, params)
+        history_training['accuracy'].append((history_train[0]))
+        history_training['loss'].append((history_train[1]))
+
+        history_val = (validate(val_loader, model, criterion, epoch, params))
+        history_validation['accuracy'].append((history_val[0]))
+        history_validation['loss'].append((history_val[1]))
+
+
+    # Вывод графиков
+    fig = plt.figure(figsize=(16, 4))
+    ax = fig.add_subplot(121)
+
+    ax.plot(list(map(np.mean, history_training['accuracy'])), label='train')
+    ax.plot(list(map(np.mean, history_validation['accuracy'])), label='test')
+    ax.set_title('График точности "Accuracy"')
+    ax.set_ylabel('accuracy')
+    ax.set_xlabel('epoch')
+    ax.legend(['train', 'test'], loc='upper left')
+    ax.grid(True)
+
+
+    fig = plt.figure(figsize=(16, 4))
+    ax = fig.add_subplot(121)
+    ax.plot(list(map(np.mean, history_training['loss'])), label='train')
+    ax.plot(list(map(np.mean, history_validation['loss'])), label='test')
+    ax.set_title('График функции потерь "Loss"')
+    ax.set_ylabel('accuracy')
+    ax.set_xlabel('epoch')
+    ax.legend(['train', 'test'], loc='upper left')
+    ax.grid(True)
+    plt.show()
+
+
+
+    
+    def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+        """
+        This function prints and plots the confusion matrix.
+        Normalization can be applied by setting `normalize=True`.
+        (This function is copied from the scikit docs.)
+        """
+        plt.figure()
+        plt.imshow(cm, interpolation='nearest', cmap=cmap)
+        plt.title(title)
+        plt.colorbar()
+        tick_marks = np.arange(len(classes))
+        plt.xticks(tick_marks, classes, rotation=45)
+        plt.yticks(tick_marks, classes)
+
+        if normalize:
+            cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print(cm)
+        thresh = cm.max() / 2.
+        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+            plt.text(j, i, cm[i, j], horizontalalignment="center", 
+                    color="white" if cm[i, j] > thresh else "black")
+
+        plt.tight_layout()
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+
 
     # Тестирование
     predicted_labels = predict(model, params, test_loader)
     print((test_images_filepaths),(predicted_labels))
     display_image_grid(test_images_filepaths, predicted_labels)
 
-    y_true_test = []
-    for batch in test_loader:
+    y_pred = predict(model, params, y_true_loader)
+
+    y_true_values = []
+    for batch in train_loader:
         inputs, labels = batch
-        y_true_test.append(torch.tensor(labels))
-    print(y_true_test)
+        y_true_values.append(labels)
+
     print(predicted_labels)
-    conf_matrix = confusion_matrix(y_true_test, predicted_labels)
+    conf_matrix = confusion_matrix(y_true_values, predicted_labels)
     print("Confusion Matrix of the Test Set")
     print("-----------")
     print(conf_matrix)
-    print("Precision of the Model :\t" + str(precision_score(y_true_test, predicted_labels)))
-    print("Recall of the Model    :\t" + str(recall_score(y_true_test, predicted_labels)))
-    print("F1 Score of the Model  :\t" + str(f1_score(y_true_test, predicted_labels)))
+    print("Precision of the Model :\t" + str(precision_score(y_true_values, predicted_labels)))
+    print("Recall of the Model    :\t" + str(recall_score(y_true_values, predicted_labels)))
+    print("F1 Score of the Model  :\t" + str(f1_score(y_true_values, predicted_labels)))
